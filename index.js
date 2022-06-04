c89d32ee4e73965415e490d02be5fa0e2c817044
Parrot <2905526259@qq.com>
2022-06-04 20:36:52 +0800

    提交了再次修改的index.html文件，index.css文件，index.js文件

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.querySelector('.father').addEventListener('click', function (e) {
     document.querySelector('.son').style.display = "block"
 })
+document.querySelector('.push_father').addEventListener('mouseover', (e) => {
+    document.querySelector('ul').style.backgroundColor = " pink"
+})
+document.querySelector('.push_father').addEventListener('mouseout', (e) => {
+    document.querySelector('ul').style.backgroundColor = " green"
+})
